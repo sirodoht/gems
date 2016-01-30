07335683d545d6570be9c4f3905a57c68447e4bb
Aristotelis Dossas <teldosas@gmail.com>
2016-01-30 17:55:54 +0200

    Add combinations and fix colors

--- back/etc/colors.etc.js
+++ back/etc/colors.etc.js
@@ -2,65 +2,69 @@
  * Colors
  */
 var colors = module.exports = {
-  yellow: {rgb: "#f00"},
-  magenta: {rgb: "#f0f"},
-  cyan: {rgb: "#0ff"},
-  grey: {rgb: "#888"},
-  darkRed: {rgb: "#800"},
-  lightRed: {rgb: "#f88"},
-  darkGreen: {rgb: "#800"},
-  lightGreen: {rgb: "#8f8"},
-  darkGreen: {rgb: "#080"},
-  lightBllue: {rgb: "#88f"},
-  darkBllue: {rgb: "#008"},
+  yellow: {rgb: '#f00'},
+  magenta: {rgb: '#f0f'},
+  cyan: {rgb: '#0ff'},
+  grey: {rgb: '#888'},
+  darkRed: {rgb: '#800'},
+  lightRed: {rgb: '#f88'},
+  lightGreen: {rgb: '#8f8'},
+  darkGreen: {rgb: '#080'},
+  lightBllue: {rgb: '#88f'},
+  darkBllue: {rgb: '#008'},
 };
 
 colors.red= {
-	rgb: "#f00",
-	combine: {
-		dark: "darkRed",
-		light: "lightRed",
-		green: "yellow",
-		blue: "magenta"
-	}
-}
+  rgb: '#f00',
+  combine: {
+    dark: 'darkRed',
+    light: 'lightRed',
+    green: 'yellow',
+    blue: 'magenta'
+  },
+  basic: true
+};
 
 colors.green = {
-	rgb: "#0f0",
-	combine: {
-		dark: "darkGreen",
-		light: "lightGreen",
-		red: "yellow",
-		blue: "cyan"
-	}
-}
+  rgb: '#0f0',
+  combine: {
+    dark: 'darkGreen',
+    light: 'lightGreen',
+    red: 'yellow',
+    blue: 'cyan'
+  },
+  basic: true
+};
 
 colors.blue = {
-	rgb: "#00f",
-	combine: {
-		dark: "darkBlue",
-		light: "lightBlue",
-		red: "magenta",
-		green: "cyan"
-	}
-}
+rgb: '#00f',
+  combine: {
+    dark: 'darkBlue',
+    light: 'lightBlue',
+    red: 'magenta',
+    green: 'cyan'
+  },
+  basic: true
+};
 
 colors.dark = {
-	rgb: "#000",
-	combine: {
-		red: "darkRed",
-		green: "darkGreen",
-		blue: "darkBlue",
-		lght: "grey"
-	}
-}
+  rgb: '#000',
+  combine: {
+    red: 'darkRed',
+    green: 'darkGreen',
+    blue: 'darkBlue',
+    light: 'grey'
+  },
+  basic: true
+};
 
 colors.light = {
-	rgb: "#000",
-	combine: {
-		red: "lightRed",
-		green: "lightGreen",
-		blue: "lightBlue",
-		dark: "grey"
-	}
-}
+  rgb: '#000',
+  combine: {
+    red: 'lightRed',
+    green: 'lightGreen',
+    blue: 'lightBlue',
+    dark: 'grey'
+  },
+  basic: true
+};
